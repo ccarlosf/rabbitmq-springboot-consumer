6db1ba42ed672eb51b616f9be37e46a6b4d879ce
ccarlos <2238513100@qq.com>
2020-04-03 12:10:36 +0800

    RabbitMQ与SpringBoot2.0整合 消费端工程初始化

---
+++ src/test/java/com/ccarlos/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
